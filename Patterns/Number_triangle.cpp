7cdde190d99cba77b0efc8af7528784d82f09121
Sristi1123 <sristi1501@gmail.com>
2026-02-14 16:32:41 +0530

    new file:   Patterns/Mirror_triangle.cpp 	new file:   Patterns/Number_triangle.cpp 	new file:   Patterns/repeated_number_triangle.cpp 	modified:   Patterns/upward_triangle.cpp

--- Patterns/Number_triangle.cpp
+++ Patterns/Number_triangle.cpp
@@ -3,11 +3,12 @@
 using namespace std;
     
 int main(){
-    int n;
-    cin>>n;
+    int n=5;
     for(int i=0;i<n;i++){
-        for(int j=n-1-i;j>=0;j--){
-            cout<<"x";
+        int count=1;
+        for(int j=0;j<i+1;j++){
+            cout<<count;
+            count++;
         }
         cout<<endl;
     }
